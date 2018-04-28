1183310c6c97c88d67493c9a016839ce27a13dd3
Jasjeev <jasjeev4@gmail.com>
2018-04-27 21:49:52 -0400

    Completed downvoteComment functionality

--- server.js
+++ server.js
@@ -352,7 +352,22 @@ function upvoteComment(url, request) {
 }
 
 function downvoteComment(url, request) {
+  const id = Number(url.split('/').filter(segment => segment)[1]);
+  const username = request.body && request.body.username;
 
+  let savedComment = database.comments[id];
+  const response = {};
+
+  if (savedComment && database.users[username]) {
+    savedComment = downvote(savedComment, username);
+
+    response.body = {comment: savedComment};
+    response.status = 200;
+  } else {
+    response.status = 400;
+  }
+
+  return response;
 }
 
 function upvote(item, username) {
